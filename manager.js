0255d6a15d301a5e33048f3f2438f9ee0233ba84
JackDunne <8jdunne@stpatrick.org>
2017-11-07 14:41:07 -0600

    Add files via upload

--- manager.js
+++ manager.js
@@ -1,8 +1,22 @@
 const thing = require('./thing.js');
 
-const moduleName = function(){
-
+const Manager = function(){
+let id = "w-"+Random().digit(8);
+let workers = Random().discreteRangeIn(4,20);
+let baseSalary = Random().discreteRangeIn(4,9)*10000;
+function pay(){
+    let dailyPay = (hours/5)*wage;
+    let weeklyPay = hours*wage;
+    let yearlyPay = weeklyPay*52;
+    let payArray = [dailyPay,weeklyPay,yearlyPay];
+    if(workers <= 10){
+        yearlyPay += workers*250;
+    }
+    else{
+        for(let x = 0;x<10;)
+    }
+    return payArray;
 
 };
 
-module.exports = moduleName;
+module.exports = Manager;
--- worker.js
+++ worker.js
@@ -2,6 +2,16 @@ const Random = require('./Random.js');
 
 const Worker = function(){
     let id = "w-"+Random().digit(8);
+    let wage = Random().discreteRangeIn(8,50);
+    let hours = Random().discreteRangeIn(4,7)*5;
+    function pay(){
+        let dailyPay = (hours/5)*wage;
+        let weeklyPay = hours*wage;
+        let yearlyPay = weeklyPay*52;
+        let payArray = [dailyPay,weeklyPay,yearlyPay];
+        return payArray;
+
+    }
 
 };
 
